0353a516f0b39e1365ab41ef864e40bdf1b3de84
Robert Isaacs <robertisaacs87@yahoo.com>
2021-09-30 18:11:52 -0500

    test merge

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+function setup() {
+    noCanvas();
+    userInput = select('#userinput');
+    userInput.changed(goWiki);
+
+    function goWiki() {
+        let term = userInput.value();
+        console.log(term);
+    }
+}
