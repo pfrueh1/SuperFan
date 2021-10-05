f8c0ca72f5f2dbd7244d1c472382d926ca16f87f
Robert Isaacs <robertisaacs87@yahoo.com>
2021-10-04 20:06:39 -0500

    added button to search and established initial wikipedia api

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,40 @@
-function setup() {
-    noCanvas();
-    userInput = select('#userinput');
-    userInput.changed(goWiki);
+//Start Wiki-card Javascript
 
-    function goWiki() {
-        let term = userInput.value();
-        console.log(term);
+//Button click event
+var button = document.querySelector(".button");
+button.addEventListener("click", function(){
+        //Create a new object to interact with the server
+    var xhr = new XMLHttpRequest();
+
+    var url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='Lizzo'";
+
+    // Provide 3 arguments (GET/POST, The URL, Async True/False)
+    xhr.open('GET', url, true);
+
+    // Once request has loaded...
+    xhr.onload = function() {
+        // Parse the request into JSON
+        var data = JSON.parse(this.response);
+
+        // Log the data object
+        console.log(data);
+
+        // Log the page objects
+        console.log(data.query.pages)
+
+        // Loop through the data object
+        // Pulling out the titles of each page
+        for (var i in data.query.pages) {
+            console.log(data.query.pages[i].title);
+        }
     }
-}
+    // Send request to the server asynchronously
+    xhr.send();
+})
+
+//End Wiki-card Javascript
+
+
 const api = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&keyword=lizzo&apikey=yBLp8jR0zVL83KhDCYm0hsP4ydR9aG2w';
 
 fetch(api).then(function(response) {
